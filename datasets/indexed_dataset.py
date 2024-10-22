e6535a85239c124e4c5e92bc55893d21b4060ec7
kushagra gupta <kushagra22263@iiitd.ac.in>
2024-10-23 01:55:20 +0530

    datasets codebase

---
+++ datasets/indexed_dataset.py
@@ -0,0 +1,14 @@
+from torch.utils.data import Dataset
+from datasets.datasets import get_dataset
+
+
+class IndexedDataset(Dataset):
+    def __init__(self, args, train=True, train_transform=False):
+        self.dataset = get_dataset(args, train=train, train_transform=train_transform)
+
+    def __getitem__(self, index):
+        data, target = self.dataset[index]
+        return data, target, index
+
+    def __len__(self):
+        return len(self.dataset)
